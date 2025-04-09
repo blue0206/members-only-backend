64fd41a73e6223c2dac9376fbc02a7ee6460f885
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-09 23:11:30 +0530

    feat: add message routes to Express application

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import "dotenv/config";
 import userRouter from "./routes/user.route.js";
+import messageRouter from "./routes/message.route.js";
 
 const app = express();
 
 // Routes
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/messages", messageRouter);
 
+// Server
 const PORT = process.env.PORT ?? 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT.toString()}`);
