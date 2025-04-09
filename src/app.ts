6dfee75aec4b838d31f9e12bb55a68babdf39a5a
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-09 23:10:08 +0530

    feat: add user routes to Express application

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import "dotenv/config";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
+
+// Routes
+app.use("/api/v1/user", userRouter);
 
 const PORT = process.env.PORT ?? 8000;
 app.listen(PORT, () => {
