1a5c28637c670bb6b0c5664d2dc67fd281ca130c
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-12 01:15:27 +0530

    feat: add auth routes to the Express app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
+import authRouter from "./features/auth/auth.routes.js";
 import userRouter from "./features/users/user.route.js";
 import messageRouter from "./features/messages/message.route.js";
 
 const app = express();
 
 // Routes
+app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/messages", messageRouter);
 
