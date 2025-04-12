6dc43688769b6836c893afc518e19a229ca9e6e0
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-12 14:54:35 +0530

    feat: add POST route for user registration

--- src/features/auth/auth.routes.ts
+++ src/features/auth/auth.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { registerUser } from "./auth.controller.js";
 
 const authRouter = Router();
+
+authRouter.post("/register", registerUser);
 
 export default authRouter;
