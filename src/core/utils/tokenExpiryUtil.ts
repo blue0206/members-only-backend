61caaa01685cb7314137937b9041ceafe7305401
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-12 13:18:09 +0530

    feat: add utility function to calculate refresh token expiry date

---
+++ src/core/utils/tokenExpiryUtil.ts
@@ -0,0 +1,7 @@
+import { config } from "../config/index.js";
+import ms from "ms";
+import type { StringValue } from "ms";
+
+export default function getRefreshTokenExpiryDate(): Date {
+  return new Date(Date.now() + ms(config.REFRESH_TOKEN_EXPIRY as StringValue));
+}
