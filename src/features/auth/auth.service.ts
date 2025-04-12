29eba6a46e9ccd8174b4a275e11b50f24e174b36
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-12 11:27:32 +0530

    feat: implement AuthService with access token generation

---
+++ src/features/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { v4 as uuidv4 } from "uuid";
+import { config } from "../../core/config/index.js";
+import ms from "ms";
+import type { StringValue } from "ms";
+import type { JwtPayload } from "./auth.types.js";
+
+class AuthService {
+  generateAccessToken(payload: JwtPayload): string {
+    return jwt.sign(payload, config.ACCESS_TOKEN_SECRET, {
+      expiresIn: ms(config.ACCESS_TOKEN_EXPIRY as StringValue),
+      jwtid: uuidv4(),
+    });
+  }
+}
+
+export const authService = new AuthService();
