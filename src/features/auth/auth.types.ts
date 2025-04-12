a0f3696e8139d2c9e0d2cc95b5037f05d604d184
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-12 14:24:37 +0530

    feat: add RegisterServiceReturnType interface for user register response

--- src/features/auth/auth.types.ts
+++ src/features/auth/auth.types.ts
@@ -1,4 +1,10 @@
 import type { Role } from "@blue0206/members-only-shared-types";
+import type { User } from "../../core/db/prisma-client/client.js";
+
+export interface RegisterServiceReturnType extends Omit<User, "password"> {
+  accessToken: string;
+  refreshToken: string;
+}
 
 export interface JwtPayload {
   id: number;
