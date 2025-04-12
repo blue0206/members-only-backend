154abe08373ef257fba21a28e73f5edbf54efe0f
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-12 14:57:45 +0530

    docs: add comments explaining controller logic

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -12,13 +12,23 @@ export const registerUser = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
+  // Validate the incoming request to make sure it adheres to the
+  // API contract (RegisterRequestDto).
   const parsedBody = RegisterRequestSchema.safeParse(req.body);
+  // Throw Error if validation fails.
+  // TODO: Implement proper error handling.
   if (!parsedBody.success) {
     throw new Error("Parse failed.");
   }
+  // Extract the RegisterRequestDto object from the parsedBody
+  // and pass it to the service layer.
   const registerData: RegisterRequestDto = parsedBody.data;
   const userData: RegisterServiceReturnType =
     await authService.register(registerData);
+  // Map the data returned by the service layer to the RegisterResponseDto
+  // to adhere to API contract.
   const responseData: RegisterResponseDto = mapToRegisterResponseDto(userData);
+  // Return response.
+  // TODO: Implement proper API Response wrapper.
   res.status(201).json(responseData);
 };
