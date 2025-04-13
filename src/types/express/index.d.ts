2f97322216e3e11e82c51fa090af86217375f0fc
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-14 01:19:35 +0530

    feat: add user property to Express Request interface

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,7 +1,10 @@
+import type { JwtPayload } from '../../features/auth/auth.types.ts';
+
 declare global {
     namespace Express {
         interface Request {
             requestId?: string;
+            user?: JwtPayload;
         }
     }
 }
