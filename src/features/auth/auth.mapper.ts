f4e1eb885a9fd4a9ecd6b64858cbbf586effac98
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-14 00:22:11 +0530

    feat: add login response mapping and validation

--- src/features/auth/auth.mapper.ts
+++ src/features/auth/auth.mapper.ts
@@ -1,11 +1,18 @@
 import {
     ErrorCodes,
+    LoginResponseSchema,
     RegisterResponseSchema,
 } from '@blue0206/members-only-shared-types';
 import { mapPrismaRoleToEnumRole } from '../../core/utils/roleMapper.js';
 import { InternalServerError } from '../../core/errors/customErrors.js';
-import type { RegisterResponseDto } from '@blue0206/members-only-shared-types';
-import type { RegisterServiceReturnType } from './auth.types.js';
+import type {
+    LoginResponseDto,
+    RegisterResponseDto,
+} from '@blue0206/members-only-shared-types';
+import type {
+    LoginServiceReturnType,
+    RegisterServiceReturnType,
+} from './auth.types.js';
 
 export const mapToRegisterResponseDto = (
     userData: RegisterServiceReturnType
@@ -21,6 +28,30 @@ export const mapToRegisterResponseDto = (
         accessToken: userData.accessToken,
     };
     const parsedData = RegisterResponseSchema.safeParse(mappedData);
+    if (!parsedData.success) {
+        throw new InternalServerError(
+            'DTO Mapping Error',
+            ErrorCodes.INTERNAL_SERVER_ERROR,
+            parsedData.error.flatten()
+        );
+    }
+    return parsedData.data;
+};
+
+export const mapToLoginResponseDto = (
+    userData: LoginServiceReturnType
+): LoginResponseDto => {
+    const mappedData: LoginResponseDto = {
+        id: userData.id,
+        username: userData.username,
+        firstname: userData.firstName,
+        middlename: userData.middleName,
+        lastname: userData.lastName,
+        avatar: userData.avatar,
+        role: mapPrismaRoleToEnumRole(userData.role),
+        accessToken: userData.accessToken,
+    };
+    const parsedData = LoginResponseSchema.safeParse(mappedData);
     if (!parsedData.success) {
         throw new InternalServerError(
             'DTO Mapping Error',
