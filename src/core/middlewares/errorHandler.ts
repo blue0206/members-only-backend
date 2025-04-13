3826f1cde76e15045f6f0b2ff490080502c8472a
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-13 15:00:14 +0530

    feat: enhance error handler for payload and response as per API contract
    The error middleware has been updated with the error handling logic.
    We create error payload as per the API contract interface and send it
    to the client as a part of ApiResponseError which again, adheres to the
    response interface defined by API contract.
    We also sanitize the error for statusCode >= 500 in production.

--- src/core/middlewares/errorHandler.ts
+++ src/core/middlewares/errorHandler.ts
@@ -1,12 +1,20 @@
+import { config } from '../config/index.js';
 import { logger } from '../logger.js';
+import { AppError } from '../errors/customErrors.js';
+import { ErrorCodes } from '@blue0206/members-only-shared-types';
 import type { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import type {
+    ApiErrorPayload,
+    ApiResponseError,
+} from '@blue0206/members-only-shared-types';
 
 export const errorHandler: ErrorRequestHandler = (
     err: Error,
     req: Request,
-    _res: Response,
+    res: Response,
     _next: NextFunction
 ): void => {
+    // Log error.
     logger.error(
         {
             err,
@@ -17,4 +25,29 @@ export const errorHandler: ErrorRequestHandler = (
         },
         err.message || 'An error occurred in the error middleware.'
     );
+    // Prepare error payload as per API contract.
+    const ErrorPayload: ApiErrorPayload = {
+        message: err.message,
+        code: ErrorCodes.INTERNAL_SERVER_ERROR,
+        statusCode: 500,
+    };
+    // If error is an instance of AppError, update error payload.
+    if (err instanceof AppError) {
+        ErrorPayload.message = err.message;
+        ErrorPayload.code = err.code;
+        ErrorPayload.statusCode = err.statusCode;
+        ErrorPayload.details = err.details;
+    }
+    // Sanitize errors before sending response in production.
+    if (config.NODE_ENV === 'production' && ErrorPayload.statusCode >= 500) {
+        ErrorPayload.message = 'Internal Server Error';
+        delete ErrorPayload.details;
+    }
+    // Return api response for error.
+    const ErrorResponse: ApiResponseError = {
+        error: ErrorPayload,
+        requestId: req.requestId,
+        success: false,
+    };
+    res.status(ErrorPayload.statusCode).json(ErrorResponse);
 };
