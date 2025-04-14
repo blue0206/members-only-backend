c2d20409935fa68937db649d44bc282add3cc175
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-14 08:42:06 +0530

    feat: add CSRF verification to logout and refresh routes

--- src/features/auth/auth.routes.ts
+++ src/features/auth/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { loginUser, registerUser } from './auth.controller.js';
 import accessTokenVerification from '../../core/middlewares/accessTokenVerification.js';
+import csrfVerification from '../../core/middlewares/csrfVerification.js';
 
 const authRouter = Router();
 
@@ -8,7 +9,7 @@ authRouter.post('/register', registerUser);
 authRouter.post('/login', loginUser);
 
 // Protected routes.
-authRouter.delete('/logout', accessTokenVerification);
-authRouter.post('/refresh');
+authRouter.delete('/logout', accessTokenVerification, csrfVerification);
+authRouter.post('/refresh', csrfVerification);
 
 export default authRouter;
