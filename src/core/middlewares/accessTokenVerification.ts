2fa76d4ec4321b1672667af173707a1484b48c40
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-14 11:22:33 +0530

    refactor: improve error handling in access token verification middleware
    Instead of throwing the error, we are directly sending the response to
    the client.
    
    However, since errors potentially occur in two places, the error
    logging, and response sending logic had to be duplicated.
    
    Therefore, this commit creates a utility function inside the middleware
    for efficient, detailed error logging and sending the response.

--- src/core/middlewares/accessTokenVerification.ts
+++ src/core/middlewares/accessTokenVerification.ts
@@ -1,48 +1,87 @@
-import { UnauthorizedError } from '../errors/customErrors.js';
+import { config } from '../config/index.js';
+import { logger } from '../logger.js';
+import { ErrorCodes } from '@blue0206/members-only-shared-types';
 import jwt from 'jsonwebtoken';
 import type { Request, Response, NextFunction } from 'express';
-import { config } from '../config/index.js';
 import type { JwtPayload } from '../../features/auth/auth.types.js';
-import { ErrorCodes } from '@blue0206/members-only-shared-types';
-import { logger } from '../logger.js';
+import type {
+    ApiErrorCode,
+    ApiErrorPayload,
+} from '@blue0206/members-only-shared-types';
 
 export default function accessTokenVerification(
     req: Request,
-    _res: Response,
+    res: Response,
     next: NextFunction
 ): void {
+    // Function to log the error and directly send response.
+    const sendUnauthorizedError = (code: ApiErrorCode, message: string): void => {
+        // Log error.
+        logger.warn(
+            {
+                errorCode: code,
+                errorMessage: message,
+                requestId: req.requestId,
+                url: req.url,
+                method: req.method,
+                ip: req.ip,
+            },
+            `Access token validation failed: ${code}`
+        );
+
+        // Prepare error payload as per API contract.
+        const ErrorPayload: ApiErrorPayload = {
+            message,
+            code,
+            statusCode: 401,
+        };
+        // Send error response.
+        res.status(401).json(ErrorPayload);
+    };
+
     // Get access token from authorization header.
     const accessToken = req.headers.authorization?.split(' ')[1];
 
-    // Throw error id token is missing.
+    // Throw error if token is missing.
     if (!accessToken) {
-        throw new UnauthorizedError(
-            'Missing access token.',
-            ErrorCodes.AUTHENTICATION_REQUIRED
+        sendUnauthorizedError(
+            ErrorCodes.AUTHENTICATION_REQUIRED,
+            'Missing access token.'
         );
+        // Explicit return to assert that accessToken is not undefined.
+        return;
     }
 
-    // Verify access token and pass the payload as user to the request for further access.
+    // Verify access token and pass the token payload as user to the request for further access.
     try {
         const decodedToken = jwt.verify(accessToken, config.ACCESS_TOKEN_SECRET);
         req.user = decodedToken as JwtPayload;
+
+        // Log success
+        logger.debug(
+            { username: req.user.username, role: req.user.role, id: req.user.id },
+            'Token verification successful.'
+        );
+
+        // Token verified, forward the request.
         next();
     } catch (error: unknown) {
-        logger.error({ err: error }, 'Token verification failed.');
+        // If error is an instance of JWT-Error, send appropriate error.
         if ((error as jwt.JsonWebTokenError).name === 'TokenExpiredError') {
-            throw new UnauthorizedError(
-                'Access token has expired.',
-                ErrorCodes.EXPIRED_TOKEN
+            sendUnauthorizedError(
+                ErrorCodes.EXPIRED_TOKEN,
+                'Access token has expired.'
             );
         } else if ((error as jwt.JsonWebTokenError).name === 'JsonWebTokenError') {
-            throw new UnauthorizedError(
-                'Token verification failed.',
-                ErrorCodes.INVALID_TOKEN
+            sendUnauthorizedError(
+                ErrorCodes.INVALID_TOKEN,
+                'Token verification failed.'
             );
         }
-        throw new UnauthorizedError(
-            'Failed to authenticate token.',
-            ErrorCodes.INVALID_TOKEN
+        // Send generic error if error not instance of JWT
+        sendUnauthorizedError(
+            ErrorCodes.INVALID_TOKEN,
+            'Failed to authenticate token.'
         );
     }
 }
