c30be023fea302c3c30ded28e8adc69ed1ba0112
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-14 08:33:13 +0530

    feat: add access token verification to logout and refresh routes

--- src/features/auth/auth.routes.ts
+++ src/features/auth/auth.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { loginUser, registerUser } from './auth.controller.js';
+import accessTokenVerification from '../../core/middlewares/accessTokenVerification.js';
 
 const authRouter = Router();
 
 authRouter.post('/register', registerUser);
 authRouter.post('/login', loginUser);
+
+// Protected routes.
+authRouter.delete('/logout', accessTokenVerification);
+authRouter.post('/refresh', accessTokenVerification);
 
 export default authRouter;
