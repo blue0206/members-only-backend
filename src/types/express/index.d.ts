b569250c577f8496160971db265fc66c59a370dc
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-14 22:04:20 +0530

    refactor: update user type in Express Request to use AccessTokenPayload

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,10 +1,11 @@
-import type { JwtPayload } from '../../features/auth/auth.types.ts';
+import type { AccessTokenPayload } from '../../features/auth/auth.types.ts';
 
 declare global {
     namespace Express {
         interface Request {
             requestId?: string;
-            user?: JwtPayload;
+            // req.user is populated with user details from access token payload on successful verification.
+            user?: AccessTokenPayload;
         }
     }
 }
