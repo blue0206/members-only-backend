536909b336c014d3dfd3e8e67ba4f226962e3d86
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-14 22:00:50 +0530

    refactor: extend separate access and refresh token payloads from JWT
    Instead of having a generic JWT payload, the access token and refresh
    token payload have been created by extending the JwtPayload provided
    by jsonwebtoken library.
    This change helps make a separation of concerns and safely assert
    decoded token type and signing payload type for each token.

--- src/features/auth/auth.types.ts
+++ src/features/auth/auth.types.ts
@@ -1,5 +1,6 @@
 import type { Role } from '@blue0206/members-only-shared-types';
 import type { User } from '../../core/db/prisma-client/client.js';
+import type { JwtPayload } from 'jsonwebtoken';
 
 export interface RegisterServiceReturnType extends Omit<User, 'password'> {
     accessToken: string;
@@ -11,8 +12,12 @@ export interface LoginServiceReturnType extends User {
     refreshToken: string;
 }
 
-export interface JwtPayload {
+export interface AccessTokenPayload extends JwtPayload {
     id: number;
     username: string;
     role: Role;
+}
+
+export interface RefreshTokenPayload extends JwtPayload {
+    id: number;
 }
