c07fc7eeb6ed11cd88c946b3dc7ba71796999916
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-15 23:32:43 +0530

    feat: add refresh response DTO mapping

--- src/features/auth/auth.mapper.ts
+++ src/features/auth/auth.mapper.ts
@@ -1,22 +1,26 @@
 import {
     ErrorCodes,
     LoginResponseSchema,
+    RefreshResponseSchema,
     RegisterResponseSchema,
 } from '@blue0206/members-only-shared-types';
 import { mapPrismaRoleToEnumRole } from '../../core/utils/roleMapper.js';
 import { InternalServerError } from '../../core/errors/customErrors.js';
 import type {
     LoginResponseDto,
+    RefreshResponseDto,
     RegisterResponseDto,
 } from '@blue0206/members-only-shared-types';
 import type {
     LoginServiceReturnType,
+    RefreshServiceReturnType,
     RegisterServiceReturnType,
 } from './auth.types.js';
 
 export const mapToRegisterResponseDto = (
     userData: RegisterServiceReturnType
 ): RegisterResponseDto => {
+    // Map data.
     const mappedData: RegisterResponseDto = {
         id: userData.id,
         username: userData.username,
@@ -27,7 +31,9 @@ export const mapToRegisterResponseDto = (
         role: mapPrismaRoleToEnumRole(userData.role),
         accessToken: userData.accessToken,
     };
+    // Parse mapped data against schema.
     const parsedData = RegisterResponseSchema.safeParse(mappedData);
+    // Throw error if parsing fails.
     if (!parsedData.success) {
         throw new InternalServerError(
             'DTO Mapping Error',
@@ -35,12 +41,14 @@ export const mapToRegisterResponseDto = (
             parsedData.error.flatten()
         );
     }
+    // Return mapped, parsed data.
     return parsedData.data;
 };
 
 export const mapToLoginResponseDto = (
     userData: LoginServiceReturnType
 ): LoginResponseDto => {
+    // Map data.
     const mappedData: LoginResponseDto = {
         id: userData.id,
         username: userData.username,
@@ -51,7 +59,9 @@ export const mapToLoginResponseDto = (
         role: mapPrismaRoleToEnumRole(userData.role),
         accessToken: userData.accessToken,
     };
+    // Parse mapped data against schema.
     const parsedData = LoginResponseSchema.safeParse(mappedData);
+    // Throw error if parsing fails.
     if (!parsedData.success) {
         throw new InternalServerError(
             'DTO Mapping Error',
@@ -59,5 +69,27 @@ export const mapToLoginResponseDto = (
             parsedData.error.flatten()
         );
     }
+    // Return mapped, parsed data.
+    return parsedData.data;
+};
+
+export const mapToRefreshResponseDto = (
+    data: RefreshServiceReturnType
+): RefreshResponseDto => {
+    // Map data.
+    const mappedData: RefreshResponseDto = {
+        accessToken: data.accessToken,
+    };
+    // Parse mapped data against schema.
+    const parsedData = RefreshResponseSchema.safeParse(mappedData);
+    // Throw error if parsing fails.
+    if (!parsedData.success) {
+        throw new InternalServerError(
+            'DTO Mapping Error',
+            ErrorCodes.INTERNAL_SERVER_ERROR,
+            parsedData.error.flatten()
+        );
+    }
+    // Return mapped, parsed data.
     return parsedData.data;
 };
