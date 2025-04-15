8e87a17e08ee622254ede4ec329cd0ba291fb320
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-15 18:01:16 +0530

    feat: add jwtErrorHandler for improved JWT error handling and validation
    JWT verification requires significant amount of error handling and is
    required in multiple places.
    This wrapper function helps keep the code DRY without having to define
    the same (or slightly distinct) error handling for verification of
    access and refresh tokens.

---
+++ src/core/utils/jwtErrorHandler.ts
@@ -0,0 +1,47 @@
+import { logger } from '../logger.js';
+import { ErrorCodes } from '@blue0206/members-only-shared-types';
+import { InternalServerError, UnauthorizedError } from '../errors/customErrors.js';
+import { JsonWebTokenError } from 'jsonwebtoken';
+import { ZodError } from 'zod';
+import type {
+    AccessTokenPayload,
+    RefreshTokenPayload,
+} from '../../features/auth/auth.types.js';
+
+export default function jwtErrorHandler<
+    TokenType extends AccessTokenPayload | RefreshTokenPayload,
+>(verifyJwt: () => TokenType): TokenType {
+    try {
+        const decodedToken: TokenType = verifyJwt();
+        return decodedToken;
+    } catch (error: unknown) {
+        // If error is an instance of JWT-Error, send appropriate error.
+        if (error instanceof JsonWebTokenError) {
+            if (error.name === 'TokenExpiredError') {
+                throw new UnauthorizedError(
+                    'Access token has expired.',
+                    ErrorCodes.EXPIRED_TOKEN
+                );
+            } else if (error.name === 'JsonWebTokenError') {
+                throw new UnauthorizedError(
+                    'Token verification failed.',
+                    ErrorCodes.INVALID_TOKEN
+                );
+            }
+        } else if (error instanceof ZodError) {
+            // This will be thrown when decoded token parsing
+            // against Zod schema fails.
+            logger.error(
+                { error: error.flatten() },
+                'JWT Payload validation failed.'
+            );
+            throw new InternalServerError('Internal server error processing token.');
+        }
+
+        // Send generic error if error not instance of JWT.
+        throw new UnauthorizedError(
+            'Failed to authenticate token.',
+            ErrorCodes.INVALID_TOKEN
+        );
+    }
+}
