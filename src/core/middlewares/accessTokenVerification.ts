188a11b095a8ab46cf9d5e3fd9d7f0cb79882b49
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-15 18:08:50 +0530

    feat: enhance access token verification with type safety using zod
    See commit d1c63bfdd63306ae31e9158667ffc202ba134525 for details.

--- src/core/middlewares/accessTokenVerification.ts
+++ src/core/middlewares/accessTokenVerification.ts
@@ -1,87 +1,82 @@
 import { config } from '../config/index.js';
 import { logger } from '../logger.js';
-import { ErrorCodes } from '@blue0206/members-only-shared-types';
 import jwt from 'jsonwebtoken';
+import { ErrorCodes } from '@blue0206/members-only-shared-types';
+import { UnauthorizedError } from '../errors/customErrors.js';
+import jwtErrorHandler from '../utils/jwtErrorHandler.js';
+import { AccessTokenPayloadSchema } from '../../features/auth/auth.types.js';
+import type { AccessTokenPayload } from '../../features/auth/auth.types.js';
+import type { ApiErrorPayload } from '@blue0206/members-only-shared-types';
 import type { Request, Response, NextFunction } from 'express';
-import type { JwtPayload } from '../../features/auth/auth.types.js';
-import type {
-    ApiErrorCode,
-    ApiErrorPayload,
-} from '@blue0206/members-only-shared-types';
 
 export default function accessTokenVerification(
     req: Request,
     res: Response,
     next: NextFunction
 ): void {
-    // Function to log the error and directly send response.
-    const sendUnauthorizedError = (code: ApiErrorCode, message: string): void => {
-        // Log error.
-        logger.warn(
-            {
-                errorCode: code,
-                errorMessage: message,
-                requestId: req.requestId,
-                url: req.url,
-                method: req.method,
-                ip: req.ip,
-            },
-            `Access token validation failed: ${code}`
-        );
-
-        // Prepare error payload as per API contract.
-        const ErrorPayload: ApiErrorPayload = {
-            message,
-            code,
-            statusCode: 401,
-        };
-        // Send error response.
-        res.status(401).json(ErrorPayload);
-    };
-
     // Get access token from authorization header.
     const accessToken = req.headers.authorization?.split(' ')[1];
 
-    // Throw error if token is missing.
-    if (!accessToken) {
-        sendUnauthorizedError(
-            ErrorCodes.AUTHENTICATION_REQUIRED,
-            'Missing access token.'
-        );
-        // Explicit return to assert that accessToken is not undefined.
-        return;
-    }
-
-    // Verify access token and pass the token payload as user to the request for further access.
+    // Verify access token and pass the token payload the request for further access
+    // of user details in protected routes.
     try {
-        const decodedToken = jwt.verify(accessToken, config.ACCESS_TOKEN_SECRET);
-        req.user = decodedToken as JwtPayload;
+        req.user = jwtErrorHandler((): AccessTokenPayload => {
+            // Throw error if token is missing. Handled in catch statement.
+            if (!accessToken) {
+                throw new UnauthorizedError(
+                    'Missing access token.',
+                    ErrorCodes.AUTHENTICATION_REQUIRED
+                );
+            }
+            // Decode token and populate user details in request.
+            const decodedToken = jwt.verify(accessToken, config.ACCESS_TOKEN_SECRET);
+
+            // Parse the decoded token against the defined zod schema. This approach
+            // is more type safe and asserts consistency in payloads across signing and verification.
+            const parsedToken = AccessTokenPayloadSchema.parse(decodedToken);
+
+            // Return correctly typed parsed token.
+            return parsedToken;
+        });
 
         // Log success
         logger.debug(
-            { username: req.user.username, role: req.user.role, id: req.user.id },
+            {
+                username: req.user.username,
+                role: req.user.role,
+                id: req.user.id,
+            },
             'Token verification successful.'
         );
 
         // Token verified, forward the request.
         next();
     } catch (error: unknown) {
-        // If error is an instance of JWT-Error, send appropriate error.
-        if ((error as jwt.JsonWebTokenError).name === 'TokenExpiredError') {
-            sendUnauthorizedError(
-                ErrorCodes.EXPIRED_TOKEN,
-                'Access token has expired.'
+        // Log error and directly send response for unauthorized errors.
+        // This indicates that verification has failed.
+        if (error instanceof UnauthorizedError) {
+            // Log error.
+            logger.warn(
+                {
+                    errorCode: error.code,
+                    errorMessage: error.message,
+                    requestId: req.requestId,
+                    url: req.url,
+                    method: req.method,
+                    ip: req.ip,
+                },
+                `Access token validation failed: ${error.code}`
             );
-        } else if ((error as jwt.JsonWebTokenError).name === 'JsonWebTokenError') {
-            sendUnauthorizedError(
-                ErrorCodes.INVALID_TOKEN,
-                'Token verification failed.'
-            );
+            // Prepare error payload as per API contract.
+            const ErrorPayload: ApiErrorPayload = {
+                message: error.message,
+                code: error.code,
+                statusCode: 401,
+            };
+            // Send error response.
+            res.status(401).json(ErrorPayload);
         }
-        // Send generic error if error not instance of JWT
-        sendUnauthorizedError(
-            ErrorCodes.INVALID_TOKEN,
-            'Failed to authenticate token.'
-        );
+        // Forward to error middleware if error not instance of UnauthorizedError
+        next(error);
     }
 }
