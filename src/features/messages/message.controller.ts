a9391ef1bb2e0a3a8793f31f339d021236b13e24
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-19 23:46:05 +0530

    feat: add getMessagesWithAuthor controller

--- src/features/messages/message.controller.ts
+++ src/features/messages/message.controller.ts
@@ -4,9 +4,13 @@ import type { Request, Response } from 'express';
 import type { GetMessagesServiceReturnType } from './message.types.js';
 import type {
     ApiResponse,
+    GetMessagesResponseDto,
     GetMessagesWithoutAuthorResponseDto,
 } from '@blue0206/members-only-shared-types';
-import { mapToGetMessagesWithoutAuthorResponseDto } from './message.mapper.js';
+import {
+    mapToGetMessagesResponseDto,
+    mapToGetMessagesWithoutAuthorResponseDto,
+} from './message.mapper.js';
 
 export const getMessagesWithoutAuthor = async (
     req: Request,
@@ -29,6 +33,37 @@ export const getMessagesWithoutAuthor = async (
 
     // Create success response object adhering with API Contract.
     const successResponse: ApiResponse<GetMessagesWithoutAuthorResponseDto> = {
+        success: true,
+        data: mappedMessages,
+        requestId: req.requestId,
+        statusCode: 200,
+    };
+
+    // Send success response.
+    res.status(200).json(successResponse);
+};
+
+export const getMessagesWithAuthor = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    // Throw error if request id is missing.
+    if (!req.requestId) {
+        throw new InternalServerError(
+            'Internal server configuration error: Missing Request ID'
+        );
+    }
+
+    // Get messages from service layer.
+    const messages: GetMessagesServiceReturnType =
+        await messageService.getMessages();
+
+    // Map service return type to DTO to adhere with API contract.
+    const mappedMessages: GetMessagesResponseDto =
+        mapToGetMessagesResponseDto(messages);
+
+    // Create success response object adhering with API Contract.
+    const successResponse: ApiResponse<GetMessagesResponseDto> = {
         success: true,
         data: mappedMessages,
         requestId: req.requestId,
