65f842e05bc7e83e94d2886d7290bfd3467dc158
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-19 23:14:40 +0530

    feat: implement getMessagesWithoutAuthor controller

---
+++ src/features/messages/message.controller.ts
@@ -0,0 +1,40 @@
+import { messageService } from './message.service.js';
+import { InternalServerError } from '../../core/errors/customErrors.js';
+import type { Request, Response } from 'express';
+import type { GetMessagesServiceReturnType } from './message.types.js';
+import type {
+    ApiResponse,
+    GetMessagesWithoutAuthorResponseDto,
+} from '@blue0206/members-only-shared-types';
+import { mapToGetMessagesWithoutAuthorResponseDto } from './message.mapper.js';
+
+export const getMessagesWithoutAuthor = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    // Throw error if request id is missing.
+    if (!req.requestId) {
+        throw new InternalServerError(
+            'Internal server configuration error: Missing Request ID'
+        );
+    }
+
+    // Get messages from service layer.
+    const messages: GetMessagesServiceReturnType =
+        await messageService.getMessages();
+
+    // Map service return type to DTO to adhere with API contract.
+    const mappedMessages: GetMessagesWithoutAuthorResponseDto =
+        mapToGetMessagesWithoutAuthorResponseDto(messages);
+
+    // Create success response object adhering with API Contract.
+    const successResponse: ApiResponse<GetMessagesWithoutAuthorResponseDto> = {
+        success: true,
+        data: mappedMessages,
+        requestId: req.requestId,
+        statusCode: 200,
+    };
+
+    // Send success response.
+    res.status(200).json(successResponse);
+};
