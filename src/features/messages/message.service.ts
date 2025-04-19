a609f2f4193665f77d3436be730d88dd152f932d
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-19 23:02:57 +0530

    feat: implement MessageService with method to get messages

---
+++ src/features/messages/message.service.ts
@@ -0,0 +1,20 @@
+import { prisma } from '../../core/db/prisma.js';
+import { logger } from '../../core/logger.js';
+import type { GetMessagesServiceReturnType } from './message.types.js';
+
+class MessageService {
+    async getMessages(): Promise<GetMessagesServiceReturnType> {
+        // Log the start of process.
+        logger.info('Getting messages from database.');
+
+        // Get messages from DB.
+        const messages: GetMessagesServiceReturnType =
+            await prisma.message.findMany();
+
+        // Log the success of process and return data.
+        logger.info('Messages fetched successfully.');
+        return messages;
+    }
+}
+
+export const messageService = new MessageService();
