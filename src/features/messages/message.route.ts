0f58508b3c921647f0d492387850c7c882b4928e
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-19 23:15:20 +0530

    feat: add public route to get all messages without author names

--- src/features/messages/message.route.ts
+++ src/features/messages/message.route.ts
@@ -1,5 +1,8 @@
-import { Router } from "express";
+import { Router } from 'express';
+import { getMessagesWithoutAuthor } from './message.controller.js';
 
 const messageRouter = Router();
+
+messageRouter.get('/public', getMessagesWithoutAuthor); // Get all messages (unregistered/user)
 
 export default messageRouter;
