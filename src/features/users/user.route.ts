0b1df621775b78995921c08e0f4a93057bbcf225
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-19 12:58:40 +0530

    feat: add protected route for fetching user messages

--- src/features/users/user.route.ts
+++ src/features/users/user.route.ts
@@ -1,5 +1,10 @@
-import { Router } from "express";
+import { Router } from 'express';
+import accessTokenVerification from '../../core/middlewares/accessTokenVerification.js';
+import { userMessages } from './user.controller.js';
 
 const userRouter = Router();
+
+// Protected Routes
+userRouter.get('/messages', accessTokenVerification, userMessages);
 
 export default userRouter;
