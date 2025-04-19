374e58424dd9ceb8d994ba6cf4b8e20e16309bcb
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-20 01:13:50 +0530

    feat: add mapper for CreateMessageResponseDto with role based mappiong

--- src/features/messages/message.mapper.ts
+++ src/features/messages/message.mapper.ts
@@ -1,13 +1,18 @@
 import {
+    CreateMessageResponseSchema,
     ErrorCodes,
     GetMessagesResponseSchema,
     GetMessagesWithoutAuthorResponseSchema,
 } from '@blue0206/members-only-shared-types';
 import type {
+    CreateMessageResponseDto,
     GetMessagesResponseDto,
     GetMessagesWithoutAuthorResponseDto,
 } from '@blue0206/members-only-shared-types';
-import type { GetMessagesServiceReturnType } from './message.types.js';
+import type {
+    CreateMessageServiceReturnType,
+    GetMessagesServiceReturnType,
+} from './message.types.js';
 import { InternalServerError } from '../../core/errors/customErrors.js';
 
 export const mapToGetMessagesWithoutAuthorResponseDto = (
@@ -56,6 +61,43 @@ export const mapToGetMessagesResponseDto = (
 
     // Parse mapped data against schema.
     const parsedData = GetMessagesResponseSchema.safeParse(mappedData);
+
+    // Throw error if parsing fails.
+    if (!parsedData.success) {
+        throw new InternalServerError(
+            'DTO Mapping Error',
+            ErrorCodes.INTERNAL_SERVER_ERROR,
+            parsedData.error.flatten()
+        );
+    }
+
+    // Return mapped, parsed data.
+    return parsedData.data;
+};
+
+export const mapToCreateMessageResponseDto = (
+    data: CreateMessageServiceReturnType
+): CreateMessageResponseDto => {
+    // Map data.
+    let mappedData: CreateMessageResponseDto;
+    if (data.author?.role === 'USER') {
+        mappedData = {
+            messageId: data.id,
+            message: data.content,
+            timestamp: data.createdAt,
+        };
+    } else {
+        mappedData = {
+            messageId: data.id,
+            message: data.content,
+            timestamp: data.createdAt,
+            username: data.author?.username,
+            edited: data.edited,
+        };
+    }
+
+    // Parse mapped data against schema.
+    const parsedData = CreateMessageResponseSchema.safeParse(mappedData);
 
     // Throw error if parsing fails.
     if (!parsedData.success) {
