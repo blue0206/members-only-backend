1035757a8a3c2c5fabb3726b3b834f9b7fa110fa
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-20 01:20:41 +0530

    feat: implement createNewMessage controller

--- src/features/messages/message.controller.ts
+++ src/features/messages/message.controller.ts
@@ -3,20 +3,30 @@ import {
     ForbiddenError,
     InternalServerError,
     UnauthorizedError,
+    ValidationError,
 } from '../../core/errors/customErrors.js';
 import {
+    CreateMessageRequestSchema,
     ErrorCodes,
     Role,
-    type ApiResponse,
-    type GetMessagesResponseDto,
-    type GetMessagesWithoutAuthorResponseDto,
 } from '@blue0206/members-only-shared-types';
 import {
+    mapToCreateMessageResponseDto,
     mapToGetMessagesResponseDto,
     mapToGetMessagesWithoutAuthorResponseDto,
 } from './message.mapper.js';
 import type { Request, Response } from 'express';
-import type { GetMessagesServiceReturnType } from './message.types.js';
+import type {
+    ApiResponse,
+    GetMessagesResponseDto,
+    GetMessagesWithoutAuthorResponseDto,
+    CreateMessageRequestDto,
+    CreateMessageResponseDto,
+} from '@blue0206/members-only-shared-types';
+import type {
+    CreateMessageServiceReturnType,
+    GetMessagesServiceReturnType,
+} from './message.types.js';
 
 export const getMessagesWithoutAuthor = async (
     req: Request,
@@ -94,4 +104,58 @@ export const getMessagesWithAuthor = async (
 
     // Send success response.
     res.status(200).json(successResponse);
+};
+
+export const createNewMessage = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    // Throw error if request id is missing.
+    if (!req.requestId) {
+        throw new InternalServerError(
+            'Internal server configuration error: Missing Request ID'
+        );
+    }
+
+    // Throw error if request object is not populated correctly by verification middleware.
+    if (!req.user) {
+        throw new UnauthorizedError(
+            'Authentication details missing.',
+            ErrorCodes.AUTHENTICATION_REQUIRED
+        );
+    }
+
+    // Validate the incoming request to make sure it adheres to the
+    // API contract (CreateMessageRequestDto).
+    const parsedBody = CreateMessageRequestSchema.safeParse(req.body);
+    // Throw Error if validation fails.
+    if (!parsedBody.success) {
+        throw new ValidationError(
+            'Invalid request body.',
+            ErrorCodes.VALIDATION_ERROR,
+            parsedBody.error.flatten()
+        );
+    }
+
+    // Extract the CreateMessageRequestDto object from the parsedBody.
+    const newMessageContent: CreateMessageRequestDto = parsedBody.data;
+
+    // Pass the extracted DTO to the service layer along with user ID from request object.
+    const createdMessage: CreateMessageServiceReturnType =
+        await messageService.createMessage(newMessageContent.message, req.user.id);
+
+    // Map service return type to DTO to adhere with API contract.
+    const mappedCreatedMessage: CreateMessageResponseDto =
+        mapToCreateMessageResponseDto(createdMessage);
+
+    // Create success response object adhering with API Contract.
+    const successResponse: ApiResponse<CreateMessageResponseDto> = {
+        success: true,
+        data: mappedCreatedMessage,
+        requestId: req.requestId,
+        statusCode: 201,
+    };
+
+    // Send success response.
+    res.status(201).json(successResponse);
 };
