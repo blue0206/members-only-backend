ed333e1f00ee91dd643a25b0d6338c4e94ba74e5
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-20 01:21:27 +0530

    feat: add route for creating new message

--- src/features/messages/message.route.ts
+++ src/features/messages/message.route.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import {
+    createNewMessage,
     getMessagesWithAuthor,
     getMessagesWithoutAuthor,
 } from './message.controller.js';
 import accessTokenVerification from '../../core/middlewares/accessTokenVerification.js';
+import csrfVerification from '../../core/middlewares/csrfVerification.js';
 
 const messageRouter = Router();
 
@@ -13,5 +15,7 @@ messageRouter.get('/public', getMessagesWithoutAuthor);
 //-----------Protected Routes-----------
 // Get all messages (Admin/Member)
 messageRouter.get('/', accessTokenVerification, getMessagesWithAuthor);
+// Create a new message.
+messageRouter.post('/', accessTokenVerification, csrfVerification, createNewMessage);
 
 export default messageRouter;
