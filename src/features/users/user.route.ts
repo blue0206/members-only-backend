538f37c8ba48bb945653e98bd074353df06fcda3
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-19 13:52:08 +0530

    feat: add PATCH route with JWT & CSRF verification to update user details

--- src/features/users/user.route.ts
+++ src/features/users/user.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import accessTokenVerification from '../../core/middlewares/accessTokenVerification.js';
-import { userMessages } from './user.controller.js';
+import csrfVerification from '../../core/middlewares/csrfVerification.js';
+import { editUser, userMessages } from './user.controller.js';
 
 const userRouter = Router();
 
 // Protected Routes
 userRouter.get('/messages', accessTokenVerification, userMessages);
+userRouter.patch('/', accessTokenVerification, csrfVerification, editUser);
 
 export default userRouter;
