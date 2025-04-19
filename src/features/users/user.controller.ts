0764abe6daa155105b97c460fcd100e3a9078773
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-19 13:50:30 +0530

    feat: implement editUser method to update user details with validation

--- src/features/users/user.controller.ts
+++ src/features/users/user.controller.ts
@@ -2,14 +2,26 @@ import { userService } from './user.service.js';
 import {
     InternalServerError,
     UnauthorizedError,
+    ValidationError,
 } from '../../core/errors/customErrors.js';
-import { mapToGetUserMessagesResponseDto } from './user.mapper.js';
-import { ErrorCodes } from '@blue0206/members-only-shared-types';
+import {
+    mapToEditUserResponseDto,
+    mapToGetUserMessagesResponseDto,
+} from './user.mapper.js';
+import {
+    EditUserRequestSchema,
+    ErrorCodes,
+} from '@blue0206/members-only-shared-types';
 import type { Request, Response } from 'express';
-import type { GetUserMessagesServiceReturnType } from './user.types.js';
+import type {
+    EditUserServiceReturnType,
+    GetUserMessagesServiceReturnType,
+} from './user.types.js';
 import type {
     GetUserMessagesResponseDto,
     ApiResponse,
+    EditUserRequestDto,
+    EditUserResponseDto,
 } from '@blue0206/members-only-shared-types';
 
 export const userMessages = async (req: Request, res: Response): Promise<void> => {
@@ -40,6 +52,59 @@ export const userMessages = async (req: Request, res: Response): Promise<void> =
     const successResponse: ApiResponse<GetUserMessagesResponseDto> = {
         success: true,
         data: userMessages,
+        requestId: req.requestId,
+        statusCode: 200,
+    };
+
+    // Send Response.
+    res.status(200).json(successResponse);
+};
+
+export const editUser = async (req: Request, res: Response): Promise<void> => {
+    // Throw error if request id is missing.
+    if (!req.requestId) {
+        throw new InternalServerError(
+            'Internal server configuration error: Missing Request ID'
+        );
+    }
+
+    // Throw error if request object is not populated correctly by verification middleware.
+    if (!req.user) {
+        throw new UnauthorizedError(
+            'Authentication details missing.',
+            ErrorCodes.AUTHENTICATION_REQUIRED
+        );
+    }
+
+    // Validate the incoming request to make sure it adheres to the
+    // API contract (EditUserRequestDto).
+    const parsedBody = EditUserRequestSchema.safeParse(req.body);
+    // Throw Error if validation fails.
+    if (!parsedBody.success) {
+        throw new ValidationError(
+            'Invalid request body.',
+            ErrorCodes.VALIDATION_ERROR,
+            parsedBody.error.flatten()
+        );
+    }
+
+    // Extract the EditUserRequestDto object from the parsedBody.
+    const editUserData: EditUserRequestDto = parsedBody.data;
+
+    // Pass the parsed DTO to the service layer.
+    const userData: EditUserServiceReturnType = await userService.editUser(
+        editUserData,
+        req.user.id
+    );
+
+    // Map the data returned by the service layer to the EditUserResponseDto
+    // to adhere to API contract.
+    const mappedUserData: EditUserResponseDto = mapToEditUserResponseDto(userData);
+
+    // Create success response object adhering with API Contract.
+    const successResponse: ApiResponse<EditUserResponseDto> = {
+        success: true,
+        data: mappedUserData,
         requestId: req.requestId,
         statusCode: 200,
     };
