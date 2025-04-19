0ca27a9db258df36f985c6209187399135b05a7e
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-19 14:40:30 +0530

    feat: add delete route for admin to delete users by username

--- src/features/users/user.route.ts
+++ src/features/users/user.route.ts
@@ -1,12 +1,20 @@
 import { Router } from 'express';
 import accessTokenVerification from '../../core/middlewares/accessTokenVerification.js';
 import csrfVerification from '../../core/middlewares/csrfVerification.js';
-import { editUser, userMessages } from './user.controller.js';
+import adminVerification from '../../core/middlewares/adminVerification.js';
+import { adminDeleteUser, editUser, userMessages } from './user.controller.js';
 
 const userRouter = Router();
 
 // Protected Routes
 userRouter.get('/messages', accessTokenVerification, userMessages);
 userRouter.patch('/', accessTokenVerification, csrfVerification, editUser);
+userRouter.delete(
+    '/:username',
+    accessTokenVerification,
+    csrfVerification,
+    adminVerification,
+    adminDeleteUser
+); // For Admin deleting other users.
 
 export default userRouter;
