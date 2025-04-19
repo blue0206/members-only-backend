11cb5bbf5b268130fb95252a62654b8e0a1e4354
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-19 23:42:49 +0530

    feat: add mapping for GetMessagesResponse DTO

--- src/features/messages/message.mapper.ts
+++ src/features/messages/message.mapper.ts
@@ -1,8 +1,12 @@
 import {
     ErrorCodes,
+    GetMessagesResponseSchema,
     GetMessagesWithoutAuthorResponseSchema,
 } from '@blue0206/members-only-shared-types';
-import type { GetMessagesWithoutAuthorResponseDto } from '@blue0206/members-only-shared-types';
+import type {
+    GetMessagesResponseDto,
+    GetMessagesWithoutAuthorResponseDto,
+} from '@blue0206/members-only-shared-types';
 import type { GetMessagesServiceReturnType } from './message.types.js';
 import { InternalServerError } from '../../core/errors/customErrors.js';
 
@@ -22,6 +26,36 @@ export const mapToGetMessagesWithoutAuthorResponseDto = (
 
     // Parse mapped data against schema.
     const parsedData = GetMessagesWithoutAuthorResponseSchema.safeParse(mappedData);
+
+    // Throw error if parsing fails.
+    if (!parsedData.success) {
+        throw new InternalServerError(
+            'DTO Mapping Error',
+            ErrorCodes.INTERNAL_SERVER_ERROR,
+            parsedData.error.flatten()
+        );
+    }
+
+    // Return mapped, parsed data.
+    return parsedData.data;
+};
+
+export const mapToGetMessagesResponseDto = (
+    messages: GetMessagesServiceReturnType
+): GetMessagesResponseDto => {
+    // Map data.
+    const mappedData: GetMessagesResponseDto = messages.map((message) => {
+        return {
+            messageId: message.id,
+            message: message.content,
+            timestamp: message.createdAt,
+            username: message.author?.username,
+            edited: message.edited,
+        };
+    });
+
+    // Parse mapped data against schema.
+    const parsedData = GetMessagesResponseSchema.safeParse(mappedData);
 
     // Throw error if parsing fails.
     if (!parsedData.success) {
