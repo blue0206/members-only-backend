c4db04fde6a23fb9470c698292ef63f52393b549
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-04-20 12:15:19 +0530

    feat: implement edit message controller with validation and DTO mapping

--- src/features/messages/message.controller.ts
+++ src/features/messages/message.controller.ts
@@ -6,10 +6,13 @@ import {
 } from '../../core/errors/customErrors.js';
 import {
     CreateMessageRequestSchema,
+    EditMessageRequestParamsSchema,
+    EditMessageRequestSchema,
     ErrorCodes,
 } from '@blue0206/members-only-shared-types';
 import {
     mapToCreateMessageResponseDto,
+    mapToEditMessageResponseDto,
     mapToGetMessagesResponseDto,
     mapToGetMessagesWithoutAuthorResponseDto,
 } from './message.mapper.js';
@@ -20,9 +23,12 @@ import type {
     GetMessagesWithoutAuthorResponseDto,
     CreateMessageRequestDto,
     CreateMessageResponseDto,
+    EditMessageRequestDto,
+    EditMessageResponseDto,
 } from '@blue0206/members-only-shared-types';
 import type {
     CreateMessageServiceReturnType,
+    EditMessageServiceReturnType,
     GetMessagesServiceReturnType,
 } from './message.types.js';
 
@@ -148,4 +154,68 @@ export const createNewMessage = async (
 
     // Send success response.
     res.status(201).json(successResponse);
+};
+
+export const editMessage = async (req: Request, res: Response): Promise<void> => {
+    // Throw error if request id is missing.
+    if (!req.requestId) {
+        throw new InternalServerError(
+            'Internal server configuration error: Missing Request ID'
+        );
+    }
+
+    // Throw error if request object is not populated correctly by verification middleware.
+    if (!req.user) {
+        throw new UnauthorizedError(
+            'Authentication details missing.',
+            ErrorCodes.AUTHENTICATION_REQUIRED
+        );
+    }
+
+    // Validate the incoming request to make sure it adheres to the
+    // API contract (EditMessageRequestDto).
+    const parsedBody = EditMessageRequestSchema.safeParse(req.body);
+    const parsedParams = EditMessageRequestParamsSchema.safeParse(req.params);
+
+    // Throw Error if validation fails.
+    if (!parsedBody.success) {
+        throw new ValidationError(
+            'Invalid request body.',
+            ErrorCodes.VALIDATION_ERROR,
+            parsedBody.error.flatten()
+        );
+    }
+    if (!parsedParams.success) {
+        throw new ValidationError(
+            'Invalid request params.',
+            ErrorCodes.VALIDATION_ERROR,
+            parsedParams.error.flatten()
+        );
+    }
+
+    // Extract the EditMessageRequestDto object from the parsedBody.
+    const messageEditData: EditMessageRequestDto = parsedBody.data;
+
+    // Pass the extracted DTO to the service layer along with user ID from request object.
+    const editedMessage: EditMessageServiceReturnType =
+        await messageService.editMessage(
+            messageEditData.newMessage,
+            parsedParams.data.messageId,
+            req.user
+        );
+
+    // Map service return type to DTO to adhere with API contract.
+    const mappedMessage: EditMessageResponseDto =
+        mapToEditMessageResponseDto(editedMessage);
+
+    // Create success response object adhering with API Contract.
+    const successResponse: ApiResponse<EditMessageResponseDto> = {
+        success: true,
+        data: mappedMessage,
+        requestId: req.requestId,
+        statusCode: 200,
+    };
+
+    // Send success response.
+    res.status(200).json(successResponse);
 };
