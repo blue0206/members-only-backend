961185666dc1aa3624e43fd1a3518c8d6fed3829
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-06-15 16:45:43 +0530

    refactor: extract clearExpiredTokens to separate module
    The `clearExpiredTokens` function, responsible for removing expired
    refresh tokens from the database, has been moved to its own dedicated file.
    
    This change improves code organization and modularity within the
    scheduler component.

---
+++ src/core/scheduler/clearExpiredTokens.ts
@@ -0,0 +1,27 @@
+import { prisma } from '../db/prisma.js';
+import { logger } from '../logger.js';
+import prismaErrorHandler from '../utils/prismaErrorHandler.js';
+
+export default async function clearExpiredTokens(): Promise<void> {
+    const now = new Date();
+
+    logger.info(`Running refresh token cleanup at ${now.toISOString()}....`);
+
+    try {
+        await prismaErrorHandler(async () => {
+            return await prisma.refreshToken.deleteMany({
+                where: {
+                    expiresAt: {
+                        lte: now,
+                    },
+                },
+            });
+        });
+        logger.info('Expired tokens cleared in scheduled job.');
+    } catch (error: unknown) {
+        logger.error(
+            { error },
+            'Error encountered in scheduled job while clearing expired tokens'
+        );
+    }
+}
--- src/core/scheduler/index.ts
+++ src/core/scheduler/index.ts
@@ -1,31 +1,5 @@
 import cron from 'node-cron';
-import prismaErrorHandler from '../utils/prismaErrorHandler.js';
-import { prisma } from '../db/prisma.js';
-import { logger } from '../logger.js';
-
-async function clearExpiredTokens(): Promise<void> {
-    const now = new Date();
-
-    logger.info(`Running refresh token cleanup at ${now.toISOString()}....`);
-
-    try {
-        await prismaErrorHandler(async () => {
-            return await prisma.refreshToken.deleteMany({
-                where: {
-                    expiresAt: {
-                        lte: now,
-                    },
-                },
-            });
-        });
-        logger.info('Expired tokens cleared in scheduled job.');
-    } catch (error: unknown) {
-        logger.error(
-            { error },
-            'Error encountered in scheduled job while clearing expired tokens'
-        );
-    }
-}
+import clearExpiredTokens from './clearExpiredTokens.js';
 
 export const clearExpiredRefreshTokensTask = cron.schedule(
     '0 0 * * *',
