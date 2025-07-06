b5e96c6bf7b159f19d25d11d84a6c4ae85370083
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-07-06 23:04:20 +0530

    feat: add AWS lambda handler for message service

---
+++ packages/message-service/src/lambda.ts
@@ -0,0 +1,13 @@
+import { logger } from '@members-only/core-utils';
+import serverlessExpress from '@codegenie/serverless-express';
+import app from './app.js';
+import type { APIGatewayProxyHandlerV2 } from 'aws-lambda';
+
+process.on('uncaughtException', (error) => {
+    logger.fatal({ err: error }, 'Uncaught Exception in Auth Service.');
+});
+process.on('unhandledRejection', (reason, promise) => {
+    logger.fatal({ err: reason, promise }, 'Unhandled Rejection in Auth Service.');
+});
+
+export const handler: APIGatewayProxyHandlerV2 = serverlessExpress({ app });
